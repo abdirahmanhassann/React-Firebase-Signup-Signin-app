58485e9f38dc53c045accc710d752bd16a70f098
abdirahmanhassann <113362995+abdirahmanhassann@users.noreply.github.com>
2022-10-11 23:56:49 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore"
 const firebaseConfig = {
-    apiKey: "AIzaSyCW12QwbTYI_T8hQKclmBtjX7y4BwEv4GY",
-    authDomain: "project-bc2bf.firebaseapp.com",
-    projectId: "project-bc2bf",
-    storageBucket: "project-bc2bf.appspot.com",
-    messagingSenderId: "487832323964",
-    appId: "1:487832323964:web:bd3b9deb1962b7dc8986f9",
-    measurementId: "G-CWXS12QJZ4"
+    //make firebase account and copy/paste api key onto here
   };
 
   const app= initializeApp(firebaseConfig);
